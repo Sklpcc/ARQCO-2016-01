b875ebc3e980e8362f9904dc7c8424e0f40ba398
Sklpcc <christian_sm21@hotmail.com>
2016-05-01 03:02:15 -0500

    Estructura principal creada, BCM e ISR implementados, SPI activo

--- ARQCO-2016-01/ARQCO-2016-01.ino
+++ ARQCO-2016-01/ARQCO-2016-01.ino
@@ -1,61 +1,135 @@
-#define shift 5
-#define storage 6
-#define dataRed 2
-#define dataGreen 3
-#define dataBlue 4
-int datas[] = {2,3,4};
-int filas[] = {22,23,24,25,26,27,28,29};
+#include <SPI.h>
 
-byte letra[] = {0x05,0x42,0x81,0x91,0x91,0x6E, 0x00, 0x00, 0x00};
-byte colors[3] = {B11111111};//[0] = r, [1] = g, [2] = b; 255 = off, 0 = on
-int actualData = 0;
+/*
+	TODO:
+	- make some animations
+	- increase the speed changing digitalWrite and similar functions within ISR
+	- add rgb support
+	- make matrix class
+	- maybe extend BCM to 8bit resolution
+	- search for inline keyword
+	- add comments
+	- test PWM instead of BCM
+*/
+#define latch	40 //pin 12 storage ST-CP
+#define enable	41 //pin 13 OE
+#define spi_ data	51 //pin 14 data DS
+#define spi_clock	52 //pin 11 SH-CP
 
-void led(int l, int r, int g, int b)
-{
-	r = constrain(r, 0, 255);
-	g = constrain(r, 0, 255);
-	b = constrain(r, 0, 255);
-}
+#define _RED	0
+#define _GREEN	1
+#define _BLUE	2
+
+#define _FIRST_CYCLE	8
+#define _SECOND_CYCLE	24
+#define _THIRD_CYCLE	56
+#define _FOURTH_CYCLE	120
+
+#define _MATRICES	1
+#define _ROWS		8
+#define _COLUMNS	(_MATRICES << 3) //leds per row
+#define _BCM_RESOLUTION 4
+#define _SPEED		0x25
+
+#define _CLOCK_SPEED_DIV_2 8000000
+
+//colors[color][bit][byte]
+//colors[r,g,b][bits - binary code modulation][8 * bytes = bits]
+byte colors[3][_BCM_RESOLUTION][_COLUMNS]; // bool colors[3][4][512];
+//I don't use bool or boolean beacuse the implementation may take up more space than expected... bool colors[3][_COLUMNS][_BCM_RESOLUTION]
+
+volatile byte bitBCM, row;
+volatile unsigned int counterBCM, rowByte, maxValueForBCMResolution; //uint8_t¿?
+SPISettings spiCONF;
 
 void setup()
 {
-	for(int i = 0; i < 3; ++i)
-	{
-		pinMode(datas[i], OUTPUT);
-		digitalWrite(datas[i], HIGH);
-	}
-	pinMode(shift,OUTPUT);
-	pinMode(storage,OUTPUT);
-	digitalWrite(shift, LOW);
-	digitalWrite(storage, LOW);
-	for(int i = 0; i < 8; ++i)
-	{
-		pinMode(filas[i], OUTPUT);
-		digitalWrite(filas[i], LOW);
-	}
+	cli(); //nointerrupts
+	
+	rowByte = 0;
+	row = 1;
+	bitBCM = 0;
+	counterBCM = 0;
+	
+	pinMode(latch, OUTPUT);
+	pinMode(enable, OUTPUT);
+	pinMode(spi_data, OUTPUT);
+	pinMode(spi_clock, OUTPUT);
+
+	setupISR();
+	setupSPI();
+	setMaxValue();
+	
+	sei();
 }
 
 void loop()
 {
-	for(int x = 0; x < 150; x++)
-	{
-		for(int c = 0; c < 8; ++c)
-		{
-			for(int f = 0; f < 8; ++f)
-				digitalWrite(filas[f], LOW);
-			if(c == 0)
-				digitalWrite(datas[actualData], LOW);
-			else
-				digitalWrite(datas[actualData], HIGH);
-			digitalWrite(shift, HIGH);
-			digitalWrite(shift, LOW);
-			digitalWrite(storage, HIGH);
-			digitalWrite(storage, LOW);
-			for(int f = 0; f < 8; ++f)
-				digitalWrite(filas[f], letra[c+1] & (1<<f));
-			delayMicroseconds(500);
-		}
-	}
-	delay(100);
-	actualData = (actualData+1)%3;
+	//animations
+	
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	digitalWrite(enable, HIGH);
+	
+	//BCM control
+	if (counterBCM == _FIRST_CYCLE || counterBCM == _SECOND_CYCLE || counterBCM == _THIRD_CYCLE)
+		bitBCM += 1;
+	
+	//Transfer
+	SPI.beginTransaction(spiCONF);
+	for (int i = 0; i < 3; ++i) //color
+		for (int j = rowByte; j < rowByte + _MATRICES; ++j)//leds per row
+			SPI.transfer(colors[i][bitBCM][j]);
+	SPI.transfer(row);
+	SPI.endTransaction();
+	
+	//Control 595
+	digitalWrite(latch, HIGH);
+	digitalWrite(latch, LOW);
+	digitalWrite(enable, LOW);
+	
+	//Increment
+	++counterBCM;
+	row <<= 1;
+	rowByte += _MATRICES;
+	
+	//Reset
+	if (counterBCM == _FOURTH_CYCLE)
+		counterBCM = bitBCM = 0;
+	if (row == 0)
+		row = 1;
+	if (rowByte == _COLUMNS)
+		rowByte = 0;
+}
+
+void setupSPI()
+{
+	spiCONF = SPISettings(_CLOCK_SPEED_DIV_2, MSBFIRST, SPI_MODE0);
+	SPI.begin();
+}
+
+void setupISR()
+{
+	TCCR1A = B00000000; //clear TCCR1A
+	TCCR1B = B00000000; //clear TCCR1B
+	TCCR1B |= ((1 << WGM12) | (1 << CS11) | (1 << CS10));
+	OCR1A  = _SPEED; //set timer
+	TIMSK1 |= (1 << OCIE1A); //enable timer
+}
+
+void setMaxValue()
+{
+	maxValueForBCMResolution = 0;
+	for(int i = 0; i < _BCM_RESOLUTION; ++i)
+		maxValueForBCMResolution |= (1<<i);
+}
+
+void clearMatrix()
+{
+	for (int i = 0; i < 3; ++i) //color
+		for(int j = 0; j < 4; ++j) //bits
+			for(int k = 0; k < _COLUMNS; ++k) //bytes
+				colors[i][j][k] = 0xFF;
 }
